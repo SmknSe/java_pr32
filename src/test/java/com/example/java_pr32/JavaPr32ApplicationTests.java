0d34f4fb7052a02d19a88dac6ac272842df2a1c3
SmknSe <semeykin2003@mail.ru>
2022-11-11 23:48:40 +0300

    Initial commit

---
+++ src/test/java/com/example/java_pr32/JavaPr32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_pr32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPr32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
